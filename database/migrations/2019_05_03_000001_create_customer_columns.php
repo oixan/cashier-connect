cd999789f7b49b8083ffd130b78fe013029c3350
oixan <oixan@live.it>
2020-04-10 20:11:25 +0200

    stripe_id column conditional

--- database/migrations/2019_05_03_000001_create_customer_columns.php
+++ database/migrations/2019_05_03_000001_create_customer_columns.php
@@ -14,7 +14,9 @@ class CreateCustomerColumns extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('stripe_id')->nullable()->index(); 
+            if (!Schema::hasColumn('users', 'stripe_id')) {
+                $table->string('stripe_id')->nullable()->index();
+            }
             $table->string('stripe_account_id')->nullable()->index();
             $table->string('card_brand')->nullable();
             $table->string('card_last_four', 4)->nullable();
