c3011935dbab230390c257b791141a915aa05ab6
oixan <oixan@live.it>
2020-04-10 20:49:38 +0200

    Roll back Migration customer

--- database/migrations/2019_05_03_000001_create_customer_columns.php
+++ database/migrations/2019_05_03_000001_create_customer_columns.php
@@ -14,21 +14,11 @@ class CreateCustomerColumns extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'stripe_id')) {
-                $table->string('stripe_id')->nullable()->index();
-            }
-            if (!Schema::hasColumn('users', 'stripe_account_id')) {
-                $table->string('stripe_account_id')->nullable()->index();
-            }
-            if (!Schema::hasColumn('users', 'card_brand')) {
-                $table->string('card_brand')->nullable();
-            }
-            if (!Schema::hasColumn('users', 'card_last_four')) {
-                $table->string('card_last_four', 4)->nullable();
-            }
-            if (!Schema::hasColumn('users', 'trial_ends_at')) {
-                $table->timestamp('trial_ends_at')->nullable();
-            }
+            $table->string('stripe_id')->nullable()->index(); 
+            $table->string('stripe_account_id')->nullable()->index();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four', 4)->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
         });
     }
 
